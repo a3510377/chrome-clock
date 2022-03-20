365e56a19bf7bfc29bdbe8b32b27252225ade055
猴子 <70706886+a3510377@users.noreply.github.com>
2022-03-20 17:45:38 +0800

    Update lave.vue

--- src/components/lave.vue
+++ src/components/lave.vue
@@ -3,25 +3,27 @@ import { onBeforeUnmount, computed, watch, ref } from "vue";
 import { useStore } from "@/store";
 
 const store = useStore();
-const countDownDate = computed(() => store.state.config.lave.laveTime);
+const countDownDate = computed(() => store.state.config.lave.laveTime || 0);
 const title = computed(() => store.state.config.lave.title || "會考剩餘倒數");
-
-watch(title, () => (document.title = title.value));
 
 const time = ref<string[]>();
 
 const timeFormat = ["天", "小時", "分鐘", "秒"];
 const func = () => {
-  let nowTime = new Date().getTime();
   let distance =
-    new Date(countDownDate.value || "2022-05-21T00:00:00+08:00").getTime() -
-    nowTime;
+    new Date(+countDownDate.value || "2022-05-21T00:00:00+08:00").getTime() -
+    new Date().getTime();
+
   time.value = [
     distance / (1e3 * 60 * 60 * 24),
     (distance % (1e3 * 60 * 60 * 24)) / (1e3 * 60 * 60),
     (distance % (1e3 * 60 * 60)) / (1e3 * 60),
     (distance % (1e3 * 60)) / 1e3,
   ].map((d) => (~~d).toString().padStart(2, "0"));
+  let data = time.value
+    .map((d, index) => [index, +d])
+    .filter(([, d]) => !(d === 0))[0];
+  document.title = `${title.value} ${data[1] || ""}${timeFormat[data[0]]}`;
 };
 func();
 
