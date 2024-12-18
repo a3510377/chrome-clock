e40d9c6267df897523f3a913983a06a5b77e2bb3
猴貓 <a102009102009@gmail.com>
2024-12-18 20:11:49 +0800

    fix var name not found

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    'postcss-variable-compress': {},
+    'postcss-variable-compress': ['x', 'y'],
     ...(process.env.NODE_ENV === 'production'
       ? { cssnano: { preset: 'default' } }
       : {}),
